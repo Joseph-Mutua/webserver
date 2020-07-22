7618dbffd431a06fafb852eea71690ded458fe97
Joseph Mutua <mutuaj793@gmail.com>
2020-07-22 20:39:02 +0000

    Add home, help,about and weather pages

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
+const log = console.log;
 
-app.get(' ', (req, res) => {
-    res.send("Hello Express!");
+app.get('', (req, res) => {
+    res.send("Hello Mutua! This is Express!");
 });
 
-app.listen(8080, 'localhost', () => {
-    console.log("Server is up on port 3000");
+app.get('/help', (req, res) => {
+    res.send('Help Page!');
+});
+
+app.get('/about', (req, res) => {
+    res.send("This is the about page");
+});
+
+app.get('/weather', (req, res) => {
+    res.send("Here you can view your weather!");
+});
+
+app.listen(8080, () => {
+    log("Server is on port 8000");
 });
