06beb94e2a567870e5717f33aa4764e6351a95d8
Joseph Mutua <mutuaj793@gmail.com>
2020-07-23 21:25:24 +0000

    Advanced templating with partials

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
+
 const app = express();
 
 //Define paths for Express config
 const publicDirPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 //Setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 //Setup static directory to serve
 app.use(express.static(publicDirPath));
@@ -30,6 +34,7 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: "Help Page",
+        name: "Joseph Mutua",
         message: "Welcome to the Help page"
     });
 });
