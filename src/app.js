2e419619b0e20b7bb4eca44a4b33b24efc52afaa
Joseph Mutua <mutuaj793@gmail.com>
2020-07-23 20:15:51 +0000

    Use hbs for templating

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ const app = express();
 const log = console.log;
 
 const publicDirPath = path.join(__dirname, '../public');
+
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: "Weather App",
+        name: "Joseph Mutua"
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: "About Me",
+        name: "Joseph Mutua"
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: "Help Page",
+        message: "Welcome to the Help page"
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send({
