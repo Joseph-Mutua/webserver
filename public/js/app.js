811543e07fcfa78cc5122e02c09756bc17a6fd0b
Joseph Mutua <mutuaj793@gmail.com>
2020-07-25 13:25:45 +0000

    Browser HTTP requests with fetch

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
-console.log("Js fILE IS NOW LOADED");
+console.log("Client side javascript is loaded!");
+
+fetch('https://104ad336dbab40fb8a0be08180087a0f.vfs.cloud9.us-east-2.amazonaws.com/weather?address=!').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error);
+        }
+        else {
+            console.log(data.location);
+            console.log(data.forecast);
+        }
+    });
+});
