148d4e94310cc493031089211ac47e93f9cafc64
Joseph Mutua <mutuaj793@gmail.com>
2020-07-25 14:38:03 +0000

    Create a search form

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,22 @@
 console.log("Client side javascript is loaded!");
 
-fetch('https://104ad336dbab40fb8a0be08180087a0f.vfs.cloud9.us-east-2.amazonaws.com/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error);
-        }
-        else {
-            console.log(data.location);
-            console.log(data.forecast);
-        }
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = search.value;
+
+    fetch(`https://104ad336dbab40fb8a0be08180087a0f.vfs.cloud9.us-east-2.amazonaws.com/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                console.log(data.error);
+            }
+            else {
+                console.log(data.location);
+                console.log(data.forecast);
+            }
+        });
     });
 });
