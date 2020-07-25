5dd79a9a59a15196789e210963afdca37ce69c34
Joseph Mutua <mutuaj793@gmail.com>
2020-07-25 15:08:57 +0000

    Add styling toinput and search button

--- public/js/app.js
+++ public/js/app.js
@@ -2,20 +2,22 @@ console.log("Client side javascript is loaded!");
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#messageOne');
+const messageTwo = document.querySelector('#messageTwo');
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
-
-    const location = search.value;
-
-    fetch(`https://104ad336dbab40fb8a0be08180087a0f.vfs.cloud9.us-east-2.amazonaws.com/weather?address=${location}`).then((response) => {
+    messageOne.textContent = "Loading...";
+    messageTwo.textContent = '';
+    const locationName = search.value;
+    fetch(`https://104ad336dbab40fb8a0be08180087a0f.vfs.cloud9.us-east-2.amazonaws.com/weather?address=${locationName}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error);
+                messageOne.textContent = data.error;
             }
             else {
-                console.log(data.location);
-                console.log(data.forecast);
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
             }
         });
     });
